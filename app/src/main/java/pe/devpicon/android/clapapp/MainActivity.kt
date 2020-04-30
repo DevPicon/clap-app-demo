1db7a89af1f94a1ee350a41c39c436a695eee50f
Armando <picondev@gmail.com>
2020-04-30 19:39:13 -0400

    base project

---
+++ app/src/main/java/pe/devpicon/android/clapapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package pe.devpicon.android.clapapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
