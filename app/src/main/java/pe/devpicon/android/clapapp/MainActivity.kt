d79ae2248357cd1e840e95026364a82eed62cc87
Armando <picondev@gmail.com>
2020-04-30 20:20:31 -0400

    add button behavior

--- app/src/main/java/pe/devpicon/android/clapapp/MainActivity.kt
+++ app/src/main/java/pe/devpicon/android/clapapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package pe.devpicon.android.clapapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var clapSound: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!this::clapSound.isInitialized) {
+            clapSound = MediaPlayer.create(this, R.raw.claps);
+        }
+
+        btn_launch.setOnClickListener {
+            clapSound.start()
+        }
     }
 }
