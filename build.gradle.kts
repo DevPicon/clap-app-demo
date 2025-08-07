967c7d0b2140c4d8c65938a7718282f9ec12f634
Armando Picón <picondev@gmail.com>
2025-08-07 14:29:14 -0500

    Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.5"
+}
+
+detekt {
+    toolVersion = "1.23.5"
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+    allRules = false
+    autoCorrect = true
 }
